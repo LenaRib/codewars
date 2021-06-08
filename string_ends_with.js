21593f4fe4e70c07c2d992d6a079a240d56cc767
Elena <2890715+LenaRib@users.noreply.github.com>
2021-06-09 00:04:18 +0200

    Update string_ends_with.js

--- string_ends_with.js
+++ string_ends_with.js
@@ -1,9 +1,9 @@
-//Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+/*Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
 
-//Examples:
+Examples:
 
-//strEndsWith('abc', 'bc') -- returns true
-//strEndsWith('abc', 'd') -- returns false
+strEndsWith('abc', 'bc') -- returns true
+strEndsWith('abc', 'd') -- returns false */
 
 export function solution(str: string, ending: string): boolean {
   return str.endsWith(ending);
