19d6673327e5d1be5ce1f1dba67331fa29bc9d3a
Elena <2890715+LenaRib@users.noreply.github.com>
2021-06-18 10:50:38 +0200

    6 kyu

---
+++ not_too_random.js
@@ -0,0 +1,26 @@
+/*Task
+You are given a sample number n. Your task is to generate a number that same as n, and comply with the following rules:
+
+Your result should be a number ;-)
+
+Your result must be random.
+
+There will be a special test that uses a 10 digits sample number to produce 1000 results, and no duplicate results are allowed.
+
+They should have the same number of digits.
+
+That is, if the sample number is 123, your result should also be a 3 digits number.
+
+They should have the same order of parity.
+
+That is, if the sample number is 123, your result should also be a 3 digits number with the order of Odd-Even-Odd. such as 347, 769, 501, etc..
+
+In this kata, digit 0 is considered as even digit. But leading zeros are not allowed.
+
+They should have the same sign.
+
+That is, if the sample number is a negative integer, your result should be a negative integer too.
+
+They should NOT have the same digit at the same index.
+
+That is, if the sample number is 123. The first digit of your result should not be 1; The second digit should not be 2; The last digit should not be 3.*/
