f0670db9d371ff683c619e22cc065075ddddc598
Elena <2890715+LenaRib@users.noreply.github.com>
2021-06-18 12:47:11 +0200

    6 kyu

--- not_too_random.js
+++ not_too_random.js
@@ -24,3 +24,35 @@ That is, if the sample number is a negative integer, your result should be a neg
 They should NOT have the same digit at the same index.
 
 That is, if the sample number is 123. The first digit of your result should not be 1; The second digit should not be 2; The last digit should not be 3.*/
+
+function notTooRandom(n){
+    let result = [], array = [];
+    let negative = n < 0 ? true : false;
+    n = n < 0 ? -n : n;
+    n += '';
+    let firstChar = +n[0];
+    
+    for (let char of n){
+      let array = +char % 2 ? [1, 3, 5, 7, 9] : [0, 2, 4, 6, 8];
+      let randomItem;
+      do {
+        randomItem = Math.floor(Math.random() * 4 );
+      } while(array[randomItem] === +char);
+        result.push(array[randomItem]);
+    }
+    
+  //check if the first number is 0 and generate new random one 
+    if(result[0] === 0){
+      array = [2, 4, 6, 8];
+      const index = array.indexOf(firstChar);
+      if (index > -1) {
+        array.splice(index, 1);
+      }
+      randomItem = Math.floor(Math.random() * 2 );
+      result[0] = (array[randomItem]);
+    }
+  
+    let number = +result.join('');
+    return negative ? -number : number;
+
+}
